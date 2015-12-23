db417e0ebb3b2a0567f07669c008f33b126e6a5f
Matt Hancock <matthancock03@gmail.com>
2015-12-22 16:52:56 -0800

    merged AJ

--- HappierHour/build/generated/source/r/debug/com/facebook/R.java
+++ HappierHour/build/generated/source/r/debug/com/facebook/R.java
@@ -92,18 +92,18 @@ public final class R {
 		public static final int box_count = 0x7f0c0048;
 		public static final int button = 0x7f0c0049;
 		public static final int center = 0x7f0c001b;
-		public static final int com_facebook_body_frame = 0x7f0c0080;
-		public static final int com_facebook_button_xout = 0x7f0c0082;
-		public static final int com_facebook_fragment_container = 0x7f0c007e;
-		public static final int com_facebook_login_activity_progress_bar = 0x7f0c007f;
-		public static final int com_facebook_tooltip_bubble_view_bottom_pointer = 0x7f0c0084;
-		public static final int com_facebook_tooltip_bubble_view_text_body = 0x7f0c0083;
-		public static final int com_facebook_tooltip_bubble_view_top_pointer = 0x7f0c0081;
+		public static final int com_facebook_body_frame = 0x7f0c0082;
+		public static final int com_facebook_button_xout = 0x7f0c0084;
+		public static final int com_facebook_fragment_container = 0x7f0c0080;
+		public static final int com_facebook_login_activity_progress_bar = 0x7f0c0081;
+		public static final int com_facebook_tooltip_bubble_view_bottom_pointer = 0x7f0c0086;
+		public static final int com_facebook_tooltip_bubble_view_text_body = 0x7f0c0085;
+		public static final int com_facebook_tooltip_bubble_view_top_pointer = 0x7f0c0083;
 		public static final int display_always = 0x7f0c004c;
 		public static final int inline = 0x7f0c004a;
 		public static final int large = 0x7f0c004e;
 		public static final int left = 0x7f0c0024;
-		public static final int messenger_send_button = 0x7f0c0091;
+		public static final int messenger_send_button = 0x7f0c0093;
 		public static final int never_display = 0x7f0c004d;
 		public static final int normal = 0x7f0c000b;
 		public static final int open_graph = 0x7f0c0045;
@@ -115,15 +115,15 @@ public final class R {
 		public static final int unknown = 0x7f0c0047;
 	}
 	public static final class layout {
-		public static final int com_facebook_activity_layout = 0x7f04001b;
-		public static final int com_facebook_login_fragment = 0x7f04001c;
-		public static final int com_facebook_tooltip_bubble = 0x7f04001d;
-		public static final int messenger_button_send_blue_large = 0x7f04002c;
-		public static final int messenger_button_send_blue_round = 0x7f04002d;
-		public static final int messenger_button_send_blue_small = 0x7f04002e;
-		public static final int messenger_button_send_white_large = 0x7f04002f;
-		public static final int messenger_button_send_white_round = 0x7f040030;
-		public static final int messenger_button_send_white_small = 0x7f040031;
+		public static final int com_facebook_activity_layout = 0x7f04001c;
+		public static final int com_facebook_login_fragment = 0x7f04001d;
+		public static final int com_facebook_tooltip_bubble = 0x7f04001e;
+		public static final int messenger_button_send_blue_large = 0x7f04002d;
+		public static final int messenger_button_send_blue_round = 0x7f04002e;
+		public static final int messenger_button_send_blue_small = 0x7f04002f;
+		public static final int messenger_button_send_white_large = 0x7f040030;
+		public static final int messenger_button_send_white_round = 0x7f040031;
+		public static final int messenger_button_send_white_small = 0x7f040032;
 	}
 	public static final class string {
 		public static final int com_facebook_image_download_unknown_error = 0x7f06000d;
--- HappierHour/build/generated/source/r/debug/com/google/android/gms/maps/R.java
+++ HappierHour/build/generated/source/r/debug/com/google/android/gms/maps/R.java
@@ -131,14 +131,14 @@ public final class R {
 		public static final int light = 0x7f0c003a;
 		public static final int none = 0x7f0c000f;
 		public static final int normal = 0x7f0c000b;
-		public static final int place_autocomplete_clear_button = 0x7f0c00a1;
-		public static final int place_autocomplete_powered_by_google = 0x7f0c00a3;
-		public static final int place_autocomplete_prediction_primary_text = 0x7f0c00a5;
-		public static final int place_autocomplete_prediction_secondary_text = 0x7f0c00a6;
-		public static final int place_autocomplete_progress = 0x7f0c00a4;
-		public static final int place_autocomplete_search_button = 0x7f0c009f;
-		public static final int place_autocomplete_search_input = 0x7f0c00a0;
-		public static final int place_autocomplete_separator = 0x7f0c00a2;
+		public static final int place_autocomplete_clear_button = 0x7f0c00a3;
+		public static final int place_autocomplete_powered_by_google = 0x7f0c00a5;
+		public static final int place_autocomplete_prediction_primary_text = 0x7f0c00a7;
+		public static final int place_autocomplete_prediction_secondary_text = 0x7f0c00a8;
+		public static final int place_autocomplete_progress = 0x7f0c00a6;
+		public static final int place_autocomplete_search_button = 0x7f0c00a1;
+		public static final int place_autocomplete_search_input = 0x7f0c00a2;
+		public static final int place_autocomplete_separator = 0x7f0c00a4;
 		public static final int satellite = 0x7f0c002e;
 		public static final int standard = 0x7f0c0036;
 		public static final int terrain = 0x7f0c002f;
@@ -149,10 +149,10 @@ public final class R {
 		public static final int google_play_services_version = 0x7f0a0005;
 	}
 	public static final class layout {
-		public static final int place_autocomplete_fragment = 0x7f04003a;
-		public static final int place_autocomplete_item_powered_by_google = 0x7f04003b;
-		public static final int place_autocomplete_item_prediction = 0x7f04003c;
-		public static final int place_autocomplete_progress = 0x7f04003d;
+		public static final int place_autocomplete_fragment = 0x7f04003b;
+		public static final int place_autocomplete_item_powered_by_google = 0x7f04003c;
+		public static final int place_autocomplete_item_prediction = 0x7f04003d;
+		public static final int place_autocomplete_progress = 0x7f04003e;
 	}
 	public static final class raw {
 	}
